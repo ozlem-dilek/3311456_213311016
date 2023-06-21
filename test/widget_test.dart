d1a41420fa71d01254f02239b542d5445e4dce68
Özlem Dilek Acar <acarozlemdilek@gmail.com>
2023-06-21 05:21:39 +0300

    updated inş

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:studyapp2/views/main.dart';
+import 'package:studyapp2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
